fc1d2e5e44992b25aafeab60a9bcc22ff1942c29
Santiago825 <santiagoorjuela812@gmail.com>
2022-11-25 23:33:28 -0500

    segundo comiit

--- proyecto_back/Config/database.js
+++ proyecto_back/Config/database.js
@@ -1,11 +1,15 @@
 const mongoose=require("mongoose");
 
-const host ="localhost";
-const port ="27017";
-const db= "Villaplast";
+const host ="usuario_so";
+const port ="12345";
+const db= "villaplast.rutxqo3.mongodb.net/?retryWrites=true&w=majority";
+
+
+
+
 
 exports.mongoConnect=()=>{
-    const mongoStringConnection=`mongodb://${host}:${port}/${db}`;
+    const mongoStringConnection=`mongodb+srv://${host}:${port}@${db}`;
     mongoose.connect(mongoStringConnection);
     mongoose.Promise=global.Promise;
     const dbConnection=mongoose.connection;
